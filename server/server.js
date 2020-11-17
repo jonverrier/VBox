c3d1fd89a57f6a2e84ed2830448f6e11b055b063
JonathanVerrier <jonathanverrier@hotmail.com>
2020-11-17 17:11:39 +0000

    Try to force https.

--- server/server.js
+++ server/server.js
@@ -63,6 +63,18 @@ app.use('/', pageRouter);
 // Routes for auth pages
 app.use('/', authRouter);
 
+function requireHTTPS(req, res, next) {
+   // The 'x-forwarded-proto' check is for Heroku
+   if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+      return res.redirect('https://' + req.get('host') + req.url);
+   }
+   next();
+}
+
+app.use(function (req, res, next) {
+   return requireHTTPS(req, res, next);
+});
+
 //redirect root to Login
 app.get('/', function (req, res) {
    return res.redirect('/login');
