eb4822ade1d202a6ba4f0abe931aad2d5318ebe0
JonathanVerrier <jonathanverrier@hotmail.com>
2021-01-02 19:48:40 +0000

    Server message replay implemented.

---
+++ server/signalmessage-model.js
@@ -0,0 +1,30 @@
+'use strict';
+// Copyright TXPCo ltd, 2020
+
+var mongoose = require("mongoose");
+
+const signalMessageSchema = new mongoose.Schema({
+   sessionId: { // can be null - which means a broadcast
+      type: String, 
+      index: true
+   },
+   sessionSubId: { // can be null - which means a broadcast
+      type: String,
+      index: true 
+   },
+   sequenceNo: {
+      type: Number,
+      required: true,
+      index: true,
+      unique : true
+   },
+   // Stores the payload as JSON to avoid multiple nested schemas for each type of payload
+   data: {
+      type: String,
+      required: true
+   }
+});
+
+const signalMessageModel = mongoose.model("SignalMessage", signalMessageSchema);
+
+module.exports = signalMessageModel;
