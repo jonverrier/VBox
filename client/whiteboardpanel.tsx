50ab5cada7bbf397d97c62d412aa02a3bc6b3fa3
JonathanVerrier <jonathanverrier@hotmail.com>
2021-02-03 16:25:43 +0000

    Building out simple whiteboard input.

--- client/whiteboardpanel.tsx
+++ client/whiteboardpanel.tsx
@@ -7,6 +7,8 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
+import Collapse from 'react-bootstrap/Collapse';
+import Button from 'react-bootstrap/Button';
 
 import * as CSS from 'csstype';
 
@@ -18,8 +20,25 @@ const thinStyle: CSS.Properties = {
    margin: '0px', padding: '0px',
 };
 
+const thinCentredStyle: CSS.Properties = {
+   margin: '0px', padding: '0px',
+   alignItems: 'center',
+   verticalAlign: 'top',
+   justifyContent: 'center'
+};
+
+const thinLeftStyle: CSS.Properties = {
+   margin: '0px', padding: '0px',
+   alignItems: 'left'
+};
+
+const popdownBtnStyle: CSS.Properties = {
+   margin: '0px', padding: '4px',
+   fontSize: '14px'
+};
+
 const whiteboardStyle: CSS.Properties = {
-   minHeight: '100%', minWidth: '320px', maxWidth: '*', color: 'white', background: 'white',
+   color: 'white', background: 'white',
    margin: '0px', padding: '0px',
    backgroundImage: 'url("board.png")',
    backgroundRepeat: 'repeat'
@@ -37,17 +56,51 @@ const whiteboardHeaderStyle: CSS.Properties = {
    backgroundRepeat: 'repeat'
 };
 
-const whiteboardElementStyle: CSS.Properties = {
+const whiteboardElementHeaderStyle: CSS.Properties = {
+   margin: '0px', padding: '0px',
    color: 'black', background: 'white',
    fontFamily: 'Permanent Marker',
-   fontSize: '32px',
-   margin: '0px', padding: '0px',
+   fontSize: '40px',
    backgroundImage: 'url("board.png")',
    backgroundRepeat: 'repeat'
 };
 
+const whiteboardElementBodyStyle: CSS.Properties = {
+   margin: '0px', padding: '0px',
+   color: 'black', background: 'white',
+   fontFamily: 'Permanent Marker',
+   fontSize: '24px',
+   backgroundImage: 'url("board.png")',
+   backgroundRepeat: 'repeat',
+   minHeight: '100%',
+   minWidth: '320px', maxWidth: '*' 
+};
+
+const blockCharStyle: CSS.Properties = {
+   margin: '0px',
+   paddingLeft: '8px', paddingRight: '8px',
+   paddingTop: '0px', paddingBottom: '0px',
+};
+
 interface IMasterWhiteboardState {
    isMounted: boolean;
+}
+
+interface IMasterWhiteboardElementProps {
+   rtc: Rtc;
+   caption: string;
+   placeholder: string;
+   initialRows: number;
+}
+
+interface IMasterWhiteboardElementState {
+   isMounted: boolean;
+   inEditMode: boolean;
+   enableOk,
+   enableCancel,
+   caption: string;
+   placeholder: string;
+   rows: number;
 }
 
 export class MasterWhiteboard extends React.Component<IConnectionProps, IMasterWhiteboardState> {
@@ -82,25 +135,90 @@ export class MasterWhiteboard extends React.Component<IConnectionProps, IMasterW
                </Col>
             </Row>
             <Row style={thinStyle}>
-               <Col style={whiteboardElementStyle}>
-                  Workout
-                                 <Form>
-                     <Form.Group controlId="idWorkout">
-                        <Form.Control as="textarea" placeholder="Type the workout details here" rows={10}
-                           backgroundImage={'url("board.png")'} backgroundRepeat={'repeat'} />
-                     </Form.Group>
-                  </Form>
+               <Col class={thinStyle}>
+                  <MasterWhiteboardElement rtc={this.props.rtc}
+                     caption={'Workout'} placeholder={'Type the workout details here.'} initialRows={10}></MasterWhiteboardElement>
                </Col>
-               <Col style={whiteboardElementStyle}>
-                  Results
-                                 <Form>
-                     <Form.Group controlId="idResults">
-                        <Form.Control as="textarea" placeholder="Type results here after the workout" rows={10} />
-                     </Form.Group>
-                  </Form>
+               <Col class={thinStyle}>
+                  <MasterWhiteboardElement rtc={this.props.rtc}
+                     caption={'Results'} placeholder={'Type results here after the workout.'} initialRows={10}></MasterWhiteboardElement>
                </Col>
             </Row>
          </div>
       );
    }
 }
+
+class MasterWhiteboardElement extends React.Component<IMasterWhiteboardElementProps, IMasterWhiteboardElementState> {
+   //member variables
+   state: IMasterWhiteboardElementState;
+
+   constructor(props: IMasterWhiteboardElementProps) {
+      super(props);
+
+      this.state = {
+         isMounted: false,
+         inEditMode: false,
+         enableOk: false,
+         enableCancel: false,
+         caption: props.caption,
+         placeholder: props.placeholder,
+         rows: props.initialRows
+      };
+
+   }
+
+   componentDidMount() {
+      // Initialise sending data to remotes
+      this.setState({ isMounted: true });
+   }
+
+   componentWillUnmount() {
+      // Stop sending data to remotes
+      this.setState({ isMounted: false });
+   }
+
+   processSave() {
+   }
+
+   processCancel() {
+   }
+
+   render() {
+      return (
+         <div>
+            <Row style={thinCentredStyle}>
+               <p style={whiteboardElementHeaderStyle}>{this.state.caption}</p><p style={blockCharStyle}></p>
+               <Button style={popdownBtnStyle} variant="secondary" size="sm" onClick={() => this.setState({ inEditMode: !this.state.inEditMode })}>&#9660;</Button>
+            </Row>      
+            <Row style={thinStyle}>
+            <Collapse in={this.state.inEditMode} style={thinLeftStyle}>
+               <div style={{ textAlign: 'right' }} >
+                  <Form>
+                     <Form.Group controlId="elementFormId">
+                        <Form.Control as="textarea" placeholder={this.state.placeholder} rows={this.state.rows} cols={60}
+                           backgroundImage={'url("board.png")'} backgroundRepeat={'repeat'} />
+                     </Form.Group>
+                     <Form.Row>
+                        <Button variant="secondary" disabled={!this.state.enableOk} className='mr'
+                           onClick={this.processSave.bind(this)}>Save</Button>
+                        <p style={blockCharStyle}></p>
+                        <Button variant="secondary" disabled={!this.state.enableCancel}
+                           onClick={this.processCancel.bind(this)}>Cancel</Button>
+                     </Form.Row>
+                  </Form>
+               </div>
+               </Collapse>
+            </Row>
+            <Row style={thinStyle}>
+               <p style={whiteboardElementBodyStyle}>{this.state.placeholder}</p>
+            </Row>
+         </div>
+      );
+   }
+}
+
+/*
+<Col style={thinStyle}>{this.state.caption}</Col>
+               <Col style={thinStyle}><Button variant="secondary" size="sm" onClick={() => this.setState({ inEditMode: !this.state.inEditMode })}>&#9660;</Button></Col>
+               */
